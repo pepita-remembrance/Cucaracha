37ecbf11c07730ffdc7ae5dbb48b82296ebd7b13
EmanuelDubor <emanueldubora@hotmail.com>
2016-09-10 14:49:53 -0300

    Mini grammar working

--- src/main/scala/ar/edu/unq/parse/tp1/Main.scala
+++ src/main/scala/ar/edu/unq/parse/tp1/Main.scala
@@ -1,10 +1,25 @@
 package ar.edu.unq.parse.tp1
 
-import org.antlr.samples.antlr4example.ExprBaseListener
+import org.antlr.samples.antlr4example._
+import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream}
 
 
 object Main extends App {
 
+  val text = "(1+1)-2\n"
+
   val lalala = new ExprBaseListener()
+
+  val inputStream = new ANTLRInputStream(text)
+
+  val lexer = new ExprLexer(inputStream)
+
+  val tokens = new CommonTokenStream(lexer)
+
+  val parser = new ExprParser(tokens)
+
+  val tree = parser.prog()
+
+  println(tree.toStringTree(parser))
 
 }
